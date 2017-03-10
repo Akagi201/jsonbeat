0c6b18e4beeb07fe29a537d66a5a59b8d65b5ba8
Akagi201 <akagi201@gmail.com>
2017-03-10 18:28:34 +0800

    feat(config): add path config

--- beater/jsonbeat.go
+++ beater/jsonbeat.go
@@ -12,13 +12,14 @@ import (
 	"github.com/Akagi201/jsonbeat/config"
 )
 
+// Jsonbeat implements the Beater interface
 type Jsonbeat struct {
-	done   chan struct{}
-	config config.Config
-	client publisher.Client
+	done   chan struct{}    // Channel used by the Run() method to stop when the Stop() method is called
+	config config.Config    // Configuration options for the Beat
+	client publisher.Client // Publisher that takes care of sending the events to the defined output
 }
 
-// Creates beater
+// New Creates the Beat object
 func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	config := config.DefaultConfig
 	if err := cfg.Unpack(&config); err != nil {
@@ -26,12 +27,14 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	}
 
 	bt := &Jsonbeat{
-		done: make(chan struct{}),
+		done:   make(chan struct{}),
 		config: config,
 	}
 	return bt, nil
 }
 
+// Run implements the Beater Run interface
+// Contains the main application loop that captures data and sends it to the defined output using the publisher
 func (bt *Jsonbeat) Run(b *beat.Beat) error {
 	logp.Info("jsonbeat is running! Hit CTRL-C to stop it.")
 
@@ -56,6 +59,8 @@ func (bt *Jsonbeat) Run(b *beat.Beat) error {
 	}
 }
 
+// Stop implements the Beater Stop interface
+// Contains logic that is called when the Beat is signaled to stop
 func (bt *Jsonbeat) Stop() {
 	bt.client.Close()
 	close(bt.done)
